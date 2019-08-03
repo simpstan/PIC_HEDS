0e6d936709e75fc37b4a0112f837573f7fb52f0b
Abetharan <31970938+Abetharan@users.noreply.github.com>
2019-08-02 17:06:55 -0700

    constants and main file

---
+++ PhysicsConstants.py
@@ -0,0 +1,6 @@
+from scipy import constants
+e = constants.value(u'elementary charge')
+mp = constants.value(u'alpha particle mass')
+me = constants.value(u'electron mass')
+kb = constants.value(u'Boltzmann constant')
+epsilon0 = 8.854187817620389e-12
---
+++ main.py
@@ -0,0 +1,1 @@
+import PhysicsConstants as const
